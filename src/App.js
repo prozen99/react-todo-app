82c8a807b82106fd42649682e94358f80c8d142f
Park, minsu <cmwmss59@gmail.com>
2024-03-08 12:09:01 +0900

    CRUD 기능 구현까지 마무리 (TODOAPP)

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import "./App.css";
 import Form from "./components/Form";
 import Lists from "./components/Lists";
  
+const initialTodoData=localStorage.getItem("todoData") ? JSON.parse
+(localStorage.getItem("todoData")):[];
+
 export default function App () {
   //state={
   //  todoData:[], //객체는 맞고
   //value: "" //객체 뒤에 ,로 자리확보해주는게 중요함.
   //};
-  const [todoData,setTodoData]=useState([]);//처음엔 빈배열로 상태정의
+  
+  const [todoData,setTodoData]=useState(initialTodoData);//처음엔 빈배열로 상태정의
   const [value,setValue]=useState(""); // 값은 처음에 비어있음.
   
 
@@ -18,6 +22,7 @@ export default function App () {
   {
     let newTodoData = todoData.filter(data => data.id !== id)
     setTodoData(newTodoData);
+    localStorage.setItem('todoData',JSON.stringify(newTodoData));
     console.log('newTodoData', newTodoData);
   };
 
@@ -36,6 +41,7 @@ const handleSubmit=(e)=> //제출 이벤트를 만드는 곳 .
   };
 
   setTodoData(prev =>[...prev,newTodo]);
+  localStorage.setItem('todoData',JSON.stringify([...todoData,newTodo]));
   setValue("");
   // spread 확산 연산자라고도 하는데 ... 은 지금 state의 todoData의 모든 상태를 newTodo의
   //상태로 변환 시키고 그값을 todoData에 다시 넣어줘 라는의미이다.
@@ -44,7 +50,11 @@ const handleSubmit=(e)=> //제출 이벤트를 만드는 곳 .
   //즉시 평가 하는 부분임.
 }
 
-
+const handleRemoveClick =()=>
+{
+  setTodoData([]);
+  localStorage.setItem('todoData',JSON.stringify([]));
+}
 
   
     return (
@@ -52,6 +62,7 @@ const handleSubmit=(e)=> //제출 이벤트를 만드는 곳 .
         <div className="w-full p-5 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg" >
           <div className="flex justify-between mb-3">
             <h1 className>할일 목록</h1>
+            <button onClick={handleRemoveClick}>Delete All</button>
           </div>
          
           <Lists  handleClick={handleClick} todoData={todoData} setTodoData={setTodoData}/>
--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const List =React.memo(({
   id,
@@ -13,30 +13,84 @@ const List =React.memo(({
 
 }) => {
 
+  const[isEditing,setIsEditing]=useState(false);
+  const[editedTitle,setEditedTitle]=useState(title);
+  // editedTitle의 값이랑 setEditedTitle의 값에다가 useState 넣어주기
+
   const handleCompleChange = (id) => // checkbox의 id를 받아서 어떤거를 바꿀건지를 정하는 부분임.
   {
     let newTodoData = todoData.map((data) => {
       if (data.id === id)// 내가 클릭한 거랑 이벤트를 받은 아이디가 같으면 . 
       {
         data.completed = !data.completed; // 누르기전에는 false였을 테니까 반전 시키는거임.
+        return data;
       }
-      return data;
+      setTodoData(newTodoData);
+      localStorage.setItem('todoData',JSON.stringify(newTodoData));
+      setIsEditing(false);
+      
+      
 
     });
     //this.setState({todoData:newTodoData}); // set State 변경 . 
     setTodoData(newTodoData);
+    localStorage.setItem('todoData',JSON.stringify(newTodoData));
   }
 
  
+  const handleEditChange=(event)=>{
+    setEditedTitle(event.target.value);
+  }
+
+const handleSubmit=(event)=>{
+  event.preventDefault();
+
+  let newTodoData=todoData.map(data =>{
+    if(data.id===id)
+    {
+      data.title=editedTitle
+    }
+    return data;
+  })
+  setTodoData(newTodoData)
+  setIsEditing(false);
+}
+
+if(isEditing)
+{
+  return(
+    <div
+    className={`
+   } flex items-center justify-between w-full px-4 py-1 my-2 bg-gray-100 text-gray-600 border rounded`}
+>
+   
+     
+       <div className="items-center">
+         <form onSubmit={handleSubmit}>
+         <input  
+          value={editedTitle}
+          onChange={handleEditChange}
+           className="w-full px-3 py-2 mr-4 text-gray-500 rounded"
+         />
+         </form>
+         
+       </div>
+       
+       <div className="items-center">
+         <button className="px-4 py-2 float-right" onClick={() => setIsEditing(false)}>x
+         </button>
 
 
-
- 
-
-
+         <button onClick={handleSubmit} className="px-4 py-2 float-right" type="submit">save
+         </button>
 
 
-  
+       </div>
+     
+   </div>
+  )
+}
+else {
   return (
     <div key={id} {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}
     className={`${
@@ -55,10 +109,25 @@ const List =React.memo(({
        <div className="items-center">
          <button className="px-4 py-2 float-right" onClick={() => handleClick(id)}>x
          </button>
+
+
+         <button className="px-4 py-2 float-right" onClick={() => setIsEditing(true)}>edit
+         </button>
+
+
        </div>
      
    </div>
   )
+}
+
+ 
+
+
+
+
+  
+  
 });
 
 export default List;
